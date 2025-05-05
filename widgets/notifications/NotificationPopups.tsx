7c69187d626dfe1711b788807aebdd3c1a5c7306
Nick Tenebruso <nickten2006@gmail.com>
2025-05-05 17:22:13 -0400

    Notification timeout

--- widgets/notifications/NotificationPopups.tsx
+++ widgets/notifications/NotificationPopups.tsx
@@ -57,7 +57,7 @@ class NotifiationMap implements Subscribable {
                                  * uncomment this if you want to "hide" the notifications
                                  * after TIMEOUT_DELAY
                                  */
-                                // this.delete(id)
+                                this.delete(id);
                             }),
                     })
                 );

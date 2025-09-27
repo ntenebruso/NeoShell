be9a919349a6b2aab3e07de89308288f81e1895c
Nick Tenebruso <nickten2006@gmail.com>
2025-09-27 10:32:05 -0400

    Changes

--- app.tsx
+++ app.tsx
@@ -9,10 +9,40 @@ import SysMenu from "./widgets/sysmenu/SysMenu";
 import { CSS_OUTPUT, generateColorScheme, generateCSS } from "./utils/styles";
 import { CONFIG_FILE } from "./utils/options";
 import { createBinding, For, This } from "ags";
+import { Gdk, Gtk } from "ags/gtk3";
 
 generateColorScheme();
 generateCSS();
 console.log(CONFIG_FILE);
+
+// function mapMonitors(
+//     widget: ({ monitor }: { monitor: Gdk.Monitor }) => Gtk.Widget
+// ) {
+//     const display = Gdk.Display.get_default();
+
+//     if (display === null) return;
+
+//     const numMonitors = display.get_n_monitors();
+//     const instances = new Map<Gdk.Monitor, Gtk.Widget>();
+
+//     for (let i = 0; i < numMonitors; i++) {
+//         const monitor = display.get_monitor(i);
+
+//         if (monitor !== null) {
+//             instances.set(monitor, widget({ monitor }));
+//         }
+//     }
+
+//     display.connect("monitor-added", (_, monitor) => {
+//         console.log("MONITOR ADDED");
+//         instances.set(monitor, widget({ monitor }));
+//     });
+
+//     display.connect("monitor-removed", (_, monitor) => {
+//         // instances.get(monitor)?.destroy();
+//         instances.delete(monitor);
+//     });
+// }
 
 function MultiMonitorWidgets() {
     const monitors = createBinding(app, "monitors");
@@ -34,10 +64,10 @@ app.start({
     instanceName: "neoshell",
     css: CSS_OUTPUT,
     main() {
+        MultiMonitorWidgets();
         Calendar();
         Launcher();
         PowerMenu();
         SysMenu();
-        MultiMonitorWidgets();
     },
 });
